10140383603ac7e4b23cf16db2efcc58c72cbc92
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-01-17 22:05:59 +0330

    icons used in navigation

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+import Search from "./Search";
+import Shopping from "./Shopping";
+import UserAccount from "./UserAccount";
+import Logo from "./Logo";
+
+export { Search, Shopping, UserAccount, Logo };
