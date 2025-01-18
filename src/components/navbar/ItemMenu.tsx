6355fc1f585c2714faa0e497d88af344154f0d71
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-01-18 16:28:32 +0330

    check current url with props value is equal with items href

--- src/components/navbar/ItemMenu.tsx
+++ src/components/navbar/ItemMenu.tsx
@@ -1,17 +1,17 @@
 "use client";
 import Link from "next/link";
+import { NavbarList } from "@component/src/types";
 import { useCheckUrls } from "@component/src/hooks";
 
-const ItemMenu = ({ navbarItem }: { navbarItem: string }) => {
-  const isSameUrl = useCheckUrls(navbarItem);
-  console.log(isSameUrl);
+const ItemMenu = ({ navbarItem }: { navbarItem: NavbarList }) => {
+  const isSameUrl = useCheckUrls(navbarItem.href);
 
   return (
     <h4
       data-url={isSameUrl}
-      className="capitalize text-gray-400 data-[url]:text-black"
+      className="capitalize text-gray-400 data-[url=true]:text-black"
     >
-      <Link href={"/" + navbarItem}>{navbarItem}</Link>
+      <Link href={navbarItem.href}>{navbarItem.name}</Link>
     </h4>
   );
 };
