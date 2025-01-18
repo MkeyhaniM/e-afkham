d767c0808cbe002b39f7aa5fc96f43a52dfa09cb
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-01-18 17:31:21 +0330

    check if your LoggedIn show properties else show login button

--- src/components/navbar/Account.tsx
+++ src/components/navbar/Account.tsx
@@ -1,5 +1,33 @@
+"use client";
+import useStore from "@component/src/store/useStore";
+import Button from "../Button";
+import { Shopping, UserAccount } from "@component/src/icons";
+import Link from "next/link";
+
 const Account = () => {
-  return <div></div>;
+  const { isLogin } = useStore();
+
+  return (
+    <div>
+      {!isLogin ? (
+        <>
+          <Button
+            name={"Login"}
+            style="bg-black text-white font-medium py-2 px-4 rounded hover:bg-gray-800"
+          />
+        </>
+      ) : (
+        <div className="flex gap-5 items-center">
+          <Link href={"/test"}>
+            <Button name={Shopping} style="" />
+          </Link>
+          <Link href={"/test"}>
+            <Button name={UserAccount} style="" />
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Account;
--- src/components/navbar/Navigation.tsx
+++ src/components/navbar/Navigation.tsx
@@ -6,7 +6,7 @@ import SearchBar from "./SearchBar";
 const Navigation = () => {
   return (
     <div className="w-full lg:py-4 lg:h-[88px]">
-      <div className="container mx-auto flex font-lexend-exa lg:gap-[40px] items-center">
+      <div className="container mx-auto flex font-lexend-exa items-center justify-evenly">
         <LogoEAfkham />
         <SearchBar />
         <Menu />
