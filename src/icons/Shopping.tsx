4d7c0dd8d5ccbdc9ea3ec8acc9437114f5ae4300
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-01-18 17:32:24 +0330

    remove comment attributes

--- src/icons/Shopping.tsx
+++ src/icons/Shopping.tsx
@@ -1,9 +1,9 @@
 const Shopping = () => {
   return (
     <svg
-      //   width="27"
-      //   height="24"
-      //   viewBox="0 0 27 24"
+      width="27"
+      height="24"
+      viewBox="0 0 27 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
--- src/icons/UserAccount.tsx
+++ src/icons/UserAccount.tsx
@@ -1,9 +1,9 @@
 const UserAccount = () => {
   return (
     <svg
-      //   width="19"
-      //   height="24"
-      //   viewBox="0 0 19 24"
+      width="19"
+      height="24"
+      viewBox="0 0 19 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
