5a2ff29206239cc1b434642fbc39a6791a316117
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-01-19 17:51:38 +0330

    added svg icons

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,5 +2,22 @@ import Search from "./Search";
 import Shopping from "./Shopping";
 import UserAccount from "./UserAccount";
 import Logo from "./Logo";
+import SmartWatch from "./SmartWatch";
+import Cameras from "./Cameras";
+import Computer from "./Computer";
+import Gaming from "./Gaming";
+import Headphone from "./Headphone";
+import Phone from "./Phone";
 
-export { Search, Shopping, UserAccount, Logo };
+export {
+  Search,
+  Shopping,
+  UserAccount,
+  Logo,
+  Phone,
+  Headphone,
+  Gaming,
+  Computer,
+  Cameras,
+  SmartWatch,
+};
