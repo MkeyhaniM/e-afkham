b3d98fe2b62551ed8a78fe60329c9108abe2b08e
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-01-22 15:43:06 +0330

    disabled no-require-imports in config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+eslintConfig.push({
+  "no-require-imports": "off",
+});
+
 export default eslintConfig;
