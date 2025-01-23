bf57dc236efb52a82751d5e54fc71582160100e1
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-01-23 19:17:11 +0330

    use wrote component in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Banner from "@/src/components/banner/Banner";
+import IntroductionProducts from "@component/src/components/IntroductionProducts/IntroductionProducts";
+
 export default function Home() {
-  return <h1>E-commerce</h1>;
+  return (
+    <div>
+      <Banner />
+      <IntroductionProducts />
+    </div>
+  );
 }
