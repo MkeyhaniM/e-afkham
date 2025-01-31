e2d03380381bb6eb2c29fbdf80a0a285d3dbf4a8
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-01-31 10:08:12 +0330

    create a footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="bg-black h-[464px]">
+      <div className="container mx-auto">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
