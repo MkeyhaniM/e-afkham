88d53ca4e9e2f7c859a6791089d09420cd803fb6
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-02-02 06:17:48 +0330

    style: change array to object array and include spacial style for each off them

--- src/constants/slideLinks.ts
+++ src/constants/slideLinks.ts
@@ -1,3 +1,20 @@
-const slideLinks = ["/", "/", "/", "/"];
+const slideLinks = [
+  {
+    style: "bg-neutral-200 text-black border border-black",
+    link: "/",
+  },
+  {
+    style: "bg-neutral-300 text-black border border-black",
+    link: "/",
+  },
+  {
+    style: "bg-neutral-400 text-black border border-black",
+    link: "/",
+  },
+  {
+    style: "bg-neutral-700 text-white border border-white",
+    link: "/",
+  },
+];
 
 export default slideLinks;
