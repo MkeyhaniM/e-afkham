500ef03df014197c35d2152a1ee9e0e0a092f5df
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-02-02 06:15:32 +0330

    feat: added Slide to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/src/components/banner/Banner";
 import Category from "@component/src/components/category/Category";
 import IntroductionProducts from "@component/src/components/IntroductionProducts/IntroductionProducts";
+import Slide from "@component/src/components/slide/Slide";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Banner />
       <IntroductionProducts />
       <Category />
+      <Slide />
     </div>
   );
 }
