a5f63937e9094283b80e2c2405f0b6001841ece0
MkeyhaniM <mahyar.keyhanii2@gmail.com>
2025-05-08 17:30:23 -0400

    feat(component): show some products are on sale

--- src/components/offPrice/ItemPrice.tsx
+++ src/components/offPrice/ItemPrice.tsx
@@ -1,11 +1,53 @@
 import {createClient} from "@/src/utils/supabase/server";
+import Offprice from "@component/src/types/OffPrice";
+import Image from "next/image";
+import Button from "../Button";
 
-const ItemPrice = async ({itemOffPrice}) => {
+interface Props {
+  itemOffPrice: Offprice;
+}
+
+const ItemPrice = async ({itemOffPrice}: Props) => {
+  if (!itemOffPrice?.id) {
+    console.error("Invalid itemOffPrice ID:", itemOffPrice);
+    return null;
+  }
+
   const supabase = await createClient();
 
-  const forginId = await supabase.from("sliders").select("*").eq("id", itemOffPrice.id).single();
+  const {data, error} = await supabase
+    .from("products")
+    .select("*")
+    .eq("product_id", itemOffPrice.id)
+    .single();
 
-  return <div>ItemPrice</div>;
+  if (error) {
+    console.error("Supabase query error:", error);
+    return null;
+  }
+
+  console.log(data);
+
+  console.log("Product data:", data);
+
+  return (
+    <div className="rounded-xl bg-[#F6F6F6] p-5 flex flex-col items-center justify-evenly gap-3 w-[320px] h-[460px]">
+      <span className="self-end">add to</span>
+      <Image
+        src={data.image_url}
+        width={200}
+        height={300}
+        alt="can not upload the picture"
+        className="w-[220px] h-[280px] "
+      />
+      <div>{data?.title}</div>
+      <h4>${data.price}</h4>
+      <Button
+        name={"Buy Now"}
+        style="bg-black text-white px-9 py-3 rounded-md hover:bg-gray-800 transition duration-200 "
+      />
+    </div>
+  );
 };
 
 export default ItemPrice;
--- src/components/offPrice/OffPrice.tsx
+++ src/components/offPrice/OffPrice.tsx
@@ -5,14 +5,27 @@ const OffPrice = async () => {
   const supabase = await createClient();
 
   const {data, error} = await supabase.from("discount_products").select();
-  console.log(data);
+
+  if (error) {
+    console.error("Supabase query error:", error);
+    return null;
+  }
+
+  console.log("Discount products data:", data);
+
+  //TODO: task
 
   return (
-    <div>
-      {data &&
-        data.map((item) => {
-          return <ItemPrice itemOffPrice={item} key={item.id} />;
-        })}
+    <div className="container mx-auto">
+      <div className="flex justify-center items-start py-32 flex-col gap-8">
+        <h3 className="text-2xl">Discounts up to -50%</h3>
+        <div className="flex gap-5 justify-center items-center ">
+          {data &&
+            data.map((item) => {
+              return <ItemPrice itemOffPrice={item} key={item.id} />;
+            })}
+        </div>
+      </div>
     </div>
   );
 };
